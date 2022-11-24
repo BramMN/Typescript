e176d3375927dfc50ea4ab69fdc2cc7dfaec219d
BramMN <Brammerten@live.nl>
2022-11-24 11:09:39 +0100

    the never type

--- typescript-basics/app.ts
+++ typescript-basics/app.ts
@@ -1,8 +1,14 @@
 let userInput: unknown
-let userName : string
+let userName: string
 
 userInput = 5
 userInput = "Bram"
 if (typeof userInput === "string") {
   userName = userInput
 }
+
+function generateError(message: string, code: number): never {
+  throw { message: message, errorCode: code }
+}
+
+generateError("An error occurred!", 500)
