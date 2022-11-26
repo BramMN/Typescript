761bb0f483dd1dc5a60aee1b9871bcefc6b9ba67
BramMN <Brammerten@live.nl>
2022-11-26 11:08:07 +0100

    shorthand initialization

--- typescript-classes-interfaces/src/app.ts
+++ typescript-classes-interfaces/src/app.ts
@@ -1,13 +1,15 @@
 class Department {
-  public name: string
+  // private id: string
+  // private name: string
   private employees: string[] = []
 
-  constructor(n: string) {
-    this.name = n
+  constructor(private id: string, public name: string) {
+    // this.id = id
+    // this.name = n
   }
 
   describe(this: Department) {
-    console.log("Department: " + this.name)
+    console.log(`Department (${this.id}): ${this.name}`)
   }
 
   addEmployee(employee: string) {
@@ -20,7 +22,7 @@ class Department {
   }
 }
 
-const accounting = new Department("Accounting")
+const accounting = new Department("d1", "Accounting")
 
 accounting.addEmployee("Bram")
 accounting.addEmployee("Job")
@@ -28,7 +30,7 @@ accounting.addEmployee("Job")
 //accounting.employees[2] = "Bart"
 
 accounting.describe()
-accounting.name = "NEW NAME"
+// accounting.name = "NEW NAME"
 accounting.printEmployeeInformation()
 
 // const accountingCopy = { name: "DUMMY", describe: accounting.describe }
