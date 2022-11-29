62ddc2a22bc8b3a61195456c9d4fb859103d61cb
BramMN <Brammerten@live.nl>
2022-11-29 11:21:36 +0100

    nullish coalescing operator

--- typescript-advanced-types/src/app.ts
+++ typescript-advanced-types/src/app.ts
@@ -45,6 +45,12 @@ const fetchedUserData = {
 
 console.log(fetchedUserData?.job?.title)
 
+const userInput = null
+
+const storedData = userInput ?? "DEFAULT"
+
+console.log(storedData)
+
 // type UnknownEmployee = Employee | Admin
 
 // function printEmployeeInformation(emp: UnknownEmployee) {
