ec25e0c9180a0ad20e9e090746a76ed7a65ae39c
BramMN <Brammerten@live.nl>
2022-11-30 11:05:37 +0100

    generic utility type

--- typescript-generics/src/app.ts
+++ typescript-generics/src/app.ts
@@ -76,3 +76,22 @@ const numberStorage = new DataStorage<number>()
 
 // objStorage.removeItem(maxObj)
 // console.log(objStorage.getItems())
+
+interface CourseGoal {
+  title: string
+  description: string
+  completeUntil: Date
+}
+
+function createCourseGoal(title: string, description: string, date: Date): CourseGoal {
+  let courseGoal: Partial<CourseGoal> = {}
+  courseGoal.title = title
+  courseGoal.description = description
+  courseGoal.completeUntil = date
+
+  return courseGoal as CourseGoal
+}
+
+const names: Readonly<string[]> = ["Bram", "Bart"]
+// names.push("Job")
+// names.pop()
