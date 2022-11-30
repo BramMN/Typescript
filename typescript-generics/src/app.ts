d0486434cd8308476f1f4cb780cbdc17a283a9f9
BramMN <Brammerten@live.nl>
2022-11-30 10:43:45 +0100

    the keyof constraint

--- typescript-generics/src/app.ts
+++ typescript-generics/src/app.ts
@@ -35,3 +35,9 @@ function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
 }
 
 console.log(countAndDescribe([]))
+
+function extractAndConvert<T extends object, U extends keyof T>(obj: T, key: U) {
+  return "Value: " + obj[key]
+}
+
+console.log(extractAndConvert({ name: "Bram" }, "name"))
