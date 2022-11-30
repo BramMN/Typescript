ff906e597364dd10e60973aa23342f07c2dbd526
BramMN <Brammerten@live.nl>
2022-11-30 10:29:59 +0100

    creating a generic function

---
+++ typescript-generics/src/app.ts
@@ -0,0 +1,19 @@
+// const names: Array<string> = ["Bram", "Job"]
+// names[0].split(" ")
+
+// const promise: Promise<number> = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve(10)
+//   }, 2000)
+// })
+
+// promise.then(data => {
+//   data.split(" ")
+// })
+
+function merge<T, U>(objA: T, objB: U) {
+  return { ...objA, ...objB }
+}
+
+const mergedObj = merge({ name: "Bram", hobbies: ["Sports"] }, { age: 25 })
+const mergedObj2 = merge({ name: "Bram" }, { age: 25 })
