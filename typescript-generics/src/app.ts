35b7bae03b8837493c3a921b2412828111e22bbb
BramMN <Brammerten@live.nl>
2022-11-30 10:56:52 +0100

    generic classes

--- typescript-generics/src/app.ts
+++ typescript-generics/src/app.ts
@@ -41,3 +41,38 @@ function extractAndConvert<T extends object, U extends keyof T>(obj: T, key: U) 
 }
 
 console.log(extractAndConvert({ name: "Bram" }, "name"))
+
+class DataStorage<T extends string | number | boolean> {
+  private data: T[] = []
+
+  addItem(item: T) {
+    this.data.push(item)
+  }
+
+  removeItem(item: T) {
+    if (this.data.indexOf(item) === -1) {
+      return
+    }
+    this.data.splice(this.data.indexOf(item), 1)
+  }
+
+  getItems() {
+    return [...this.data]
+  }
+}
+
+const textStorage = new DataStorage<string>()
+textStorage.addItem("Bram")
+textStorage.addItem("Job")
+textStorage.removeItem("Bram")
+console.log(textStorage.getItems())
+
+const numberStorage = new DataStorage<number>()
+
+// const objStorage = new DataStorage<object>()
+// const maxObj = { name: "Bram"}
+// objStorage.addItem(maxObj)
+// objStorage.addItem({ name: "Job" })
+
+// objStorage.removeItem(maxObj)
+// console.log(objStorage.getItems())
