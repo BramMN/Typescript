0f12f7ed14744ab91c2250b0cafc0f7a0d977fc6
BramMN <Brammerten@live.nl>
2022-11-30 10:40:34 +0100

    another generic function

--- typescript-generics/src/app.ts
+++ typescript-generics/src/app.ts
@@ -19,3 +19,19 @@ const mergedObj = merge({ name: "Bram", hobbies: ["Sports"] }, { age: 25 })
 const mergedObj2 = merge({ name: "Bram" }, { age: 25 })
 
 console.log(mergedObj)
+
+interface Lengthy {
+  length: number
+}
+
+function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
+  let descriptionText = "Got no value"
+  if (element.length === 1) {
+    descriptionText = "Got 1 element"
+  } else if (element.length > 1) {
+    descriptionText = "Got " + element.length + " elements"
+  }
+  return [element, descriptionText]
+}
+
+console.log(countAndDescribe([]))
