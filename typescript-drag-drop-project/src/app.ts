562004dac6ee813f96c1b8126dca641c895a8cf7
BramMN <Brammerten@live.nl>
2022-12-05 12:08:18 +0100

    finishing drag and drop

--- typescript-drag-drop-project/src/app.ts
+++ typescript-drag-drop-project/src/app.ts
@@ -47,6 +47,18 @@ class ProjectState extends State<Project> {
   addProject(title: string, description: string, people: number) {
     const newProject = new Project(Math.random().toString(), title, description, people, ProjectStatus.Active)
     this.projects.push(newProject)
+    this.updateListeners()
+  }
+
+  moveProject(projectId: string, newStatus: ProjectStatus) {
+    const project = this.projects.find(project => project.id === projectId)
+    if (project && project.status !== newStatus) {
+      project.status = newStatus
+      this.updateListeners()
+    }
+  }
+
+  private updateListeners() {
     for (const listenerFn of this.listeners) {
       listenerFn(this.projects.slice())
     }
@@ -185,8 +197,10 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement> implements Drag
     }
   }
 
+  @AutoBind
   dropHandler(event: DragEvent): void {
-    console.log(event.dataTransfer!.getData("text/plain"))
+    const projectId = event.dataTransfer!.getData("text/plain")
+    projectState.moveProject(projectId, this.type === "active" ? ProjectStatus.Active : ProjectStatus.Finished)
   }
 
   @AutoBind
