a187d4f7e3f7c05f02e227c183830844036e323b
BramMN <Brammerten@live.nl>
2022-12-07 11:51:51 +0100

    class validator for decorators

--- typescript-libraries/src/app.ts
+++ typescript-libraries/src/app.ts
@@ -2,11 +2,21 @@ import "reflect-metadata"
 import { plainToInstance } from "class-transformer"
 
 import { Product } from "./product.model"
+import { validate } from "class-validator"
 
 const products = [
   { title: "A carpet", price: 29.99 },
   { title: "A Book", price: 10.99 },
 ]
+
+const newProd = new Product("", -15.99)
+validate(newProd).then(errors => {
+  if (errors.length > 0) {
+    console.log("VALIDATION ERRORS:", errors)
+  } else {
+    console.log(newProd.getInformation())
+  }
+})
 
 // const p1 = new Product("A book", 12.99)
 
