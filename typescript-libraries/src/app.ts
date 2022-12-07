ac735a91f838dc7b9f10e983972559f44aeeaa0e
BramMN <Brammerten@live.nl>
2022-12-07 11:43:43 +0100

    class transformer package

--- typescript-libraries/src/app.ts
+++ typescript-libraries/src/app.ts
@@ -1,7 +1,21 @@
-import _ from "lodash"
+import "reflect-metadata"
+import { plainToInstance } from "class-transformer"
 
-declare var GLOBAL: string
+import { Product } from "./product.model"
 
-console.log(_.shuffle([1, 2, 3]))
+const products = [
+  { title: "A carpet", price: 29.99 },
+  { title: "A Book", price: 10.99 },
+]
 
-console.log(GLOBAL)
+// const p1 = new Product("A book", 12.99)
+
+// const loadedProducts = products.map(product => {
+//   return new Product(product.title, product.price)
+// })
+
+const loadedProducts = plainToInstance(Product, products)
+
+for (const prod of loadedProducts) {
+  console.log(prod.getInformation())
+}
