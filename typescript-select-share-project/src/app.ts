cb666e4df0ec8fba26ae33d56ee9a7c586df6ab6
BramMN <Brammerten@live.nl>
2022-12-08 11:58:50 +0100

    rendering a map

--- typescript-select-share-project/src/app.ts
+++ typescript-select-share-project/src/app.ts
@@ -3,6 +3,9 @@ import axios from "axios"
 const form = document.querySelector("form")!
 const addressInput = document.getElementById("address")! as HTMLInputElement
 
+const GOOGLE_API_KEY = ""
+
+// declare var google: any
 
 type GoogleGeocodingResponse = { results: { geometry: { location: { lat: number; lng: number } } }[]; status: "OK" | "ZERO_RESULTS" }
 
@@ -19,6 +22,12 @@ function searchAddressHandler(event: Event) {
         throw new Error("Could not fetch location!")
       }
       const coordinates = response.data.results[0].geometry.location
+      const map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
+        center: coordinates,
+        zoom: 16,
+      })
+
+      new google.maps.Marker({ position: coordinates, map: map })
     })
     .catch(error => {
       alert(error.message)
