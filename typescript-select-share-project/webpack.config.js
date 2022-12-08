06dfcbdf53d1252d1bc564b6917792a4b4e4db55
BramMN <Brammerten@live.nl>
2022-12-08 11:23:21 +0100

    project setup

--- typescript-select-share-project/webpack.config.js
+++ typescript-select-share-project/webpack.config.js
@@ -1,23 +1,29 @@
-const path = require('path');
+const path = require("path")
 
 module.exports = {
   mode: 'development',
-  entry: './src/app.ts',
+  entry: "./src/app.ts",
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: 'dist'
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    publicPath: '/dist/',
   },
+  devtool: "inline-source-map",
   module: {
     rules: [
       {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/
-      }
-    ]
+        test: /\.ts$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
   },
   resolve: {
-    extensions: ['.ts', '.js']
-  }
-};
+    extensions: [".ts", ".js"],
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "/"),
+    },
+  },
+}
