c6cd9f7434bc23bdb354c986bd34d45bad4a4fc4
BramMN <Brammerten@live.nl>
2022-12-09 11:17:17 +0100

    add styling

--- typescript-react/src/components/NewTodo.tsx
+++ typescript-react/src/components/NewTodo.tsx
@@ -1,5 +1,7 @@
 import React, { useRef } from "react"
 
+import "./NewTodo.css"
+
 interface Props {
   onAddTodo: (todoText: string) => void
 }
@@ -16,7 +18,7 @@ function NewTodo(props: Props): JSX.Element {
 
   return (
     <form onSubmit={todoSubmitHandler}>
-      <div>
+      <div className="form-control">
         <label htmlFor="todo-text">Todo Text</label>
         <input type="text" id="todo-text" ref={textInputRef} />
       </div>
--- typescript-react/src/components/TodoList.tsx
+++ typescript-react/src/components/TodoList.tsx
@@ -1,3 +1,5 @@
+import "./TodoList.css"
+
 interface Props {
   items: { id: string; text: string }[]
   onDeleteTodo: (id: string) => void
