dc50bf2d7c9b188481a4f04a3d610dba7d71f601
BramMN <Brammerten@live.nl>
2022-12-09 11:08:53 +0100

    working with state and types

--- typescript-react/src/App.tsx
+++ typescript-react/src/App.tsx
@@ -1,11 +1,13 @@
+import { useState } from "react"
 import NewTodo from "./components/NewTodo"
 import TodoList from "./components/TodoList"
+import { Todo } from "./todo.model"
 
 function App(): JSX.Element {
-  const todos = [{ id: "t1", text: "Finish the course" }]
+  const [todos, setTodos] = useState<Todo[]>([])
 
   function todoAddHandler(text: string) {
-    console.log(text)
+    setTodos([{ id: Math.random().toString(), text }])
   }
 
   return (
